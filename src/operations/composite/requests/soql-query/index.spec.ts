bb78ce85c59beb18a22e2cf056e0fc3e007d5b49
marcusyavorskyy <marcus.yavorskyy@new10.com>
2023-08-18 14:49:33 +0200

    chore: fixed test

--- src/operations/composite/requests/soql-query/index.spec.ts
+++ src/operations/composite/requests/soql-query/index.spec.ts
@@ -1,4 +1,4 @@
-import { makeCreateCompositeSubRequestUpdateSObject } from './index'
+import { makeCreateCompositeSubRequestSoqlQuery } from './index'
 
 import got, { Got } from 'got'
 import { disableNetConnect } from 'nock'
@@ -7,41 +7,31 @@ beforeAll(() => {
     disableNetConnect()
 })
 
-const sObjectName = 'Account'
 const baseUrl = 'https://bolo.example.com/'
 describe('createCompositeSubRequestUpdateSObject', () => {
-    const record = {
-        Name: 'koko-company',
-    }
-
     const gotInstance: Got = got.extend({
         prefixUrl: baseUrl,
     })
 
-    const createCompositeSubRequestUpdateSObject = makeCreateCompositeSubRequestUpdateSObject({
+    const createCompositeSubRequestSoqlQuery = makeCreateCompositeSubRequestSoqlQuery({
         gotInstance,
     })
 
     it('should create subRequest in salesforce', () => {
-        const subRequest = createCompositeSubRequestUpdateSObject({
-            sObjectName,
-            record,
-            recordId: 'account-id-1',
+        const subRequest = createCompositeSubRequestSoqlQuery({
+            queryStatement: 'SELECT Id FROM Account LIMIT 1',
             referenceId: 'koko',
         })
 
         expect(subRequest).toMatchInlineSnapshot(`
             {
-              "body": {
-                "Name": "koko-company",
-              },
               "httpHeaders": {
                 "content-type": "application/json",
                 "user-agent": "got (https://github.com/sindresorhus/got)",
               },
-              "method": "PATCH",
+              "method": "GET",
               "referenceId": "koko",
-              "url": "services/data/v55.0/sobjects/Account/account-id-1",
+              "url": "services/data/v55.0/query/?q=SELECT Id FROM Account LIMIT 1",
             }
         `)
     })
